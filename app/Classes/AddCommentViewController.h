678ef64310cba7391c314a918d970852ca13b2d1
Tom Insam <tom@movieos.org>
2011-10-07 13:47:19 +0100

    space things out more, make text bigger, make tags bigger.

---
+++ app/Classes/AddCommentViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddCommentViewController.h
+//  Noticings
+//
+//  Created by Tom Insam on 07/10/2011.
+//  Copyright (c) 2011 Strange Tractor Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddCommentViewController : UIViewController
+
+@end
---
+++ app/Classes/AddCommentViewController.m
@@ -0,0 +1,51 @@
+//
+//  AddCommentViewController.m
+//  Noticings
+//
+//  Created by Tom Insam on 07/10/2011.
+//  Copyright (c) 2011 Strange Tractor Limited. All rights reserved.
+//
+
+#import "AddCommentViewController.h"
+
+@implementation AddCommentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
